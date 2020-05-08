2ecaa0fee3cede867216bab5e0c7a12bbb51bd28
Héctor Akamine <hakamine@artefactual.com>
2020-05-08 09:35:36 -0700

    Use environs

--- project/project/settings.py
+++ project/project/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+from environs import Env
+
+
+env = Env()
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,12 +25,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'uf3c3$29xezt&%#kzyqzv$!#iq2pbmz6#o-^cgj1ajyc^op-_1'
+SECRET_KEY = env('SECRET_KEY','uf3c3$29xezt&%#kzyqzv$!#iq2pbmz6#o-^cgj1ajyc^op-_1')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG', True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", [])
 
 
 # Application definition
